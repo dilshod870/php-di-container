3d9e1608dd5a83d3e89d5077e8a9cf7dcc7cfef5
dilshod870 <dilshod61870@gmail.com>
2025-10-22 13:17:37 +0500

    Add Country model for booking system

---
+++ app/Models/Country.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+class Country {
+    private $name;
+
+    public function __construct($name) {
+        $this->name = $name;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+}
