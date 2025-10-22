afce06cdda8c5cd7441da26f74bd923609497754
dilshod870 <dilshod61870@gmail.com>
2025-10-22 13:37:46 +0500

    Update Country.php with new content

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,15 +1,20 @@
 <?php
-
 namespace App\Models;
 
-class Country {
-    private $name;
+class Country
+{
+    private int $id;
+    private string $name;
+    private string $code;
 
-    public function __construct($name) {
+    public function __construct(int $id, string $name, string $code)
+    {
+        $this->id = $id;
         $this->name = $name;
+        $this->code = strtoupper($code);
     }
 
-    public function getName() {
-        return $this->name;
-    }
+    public function getId(): int { return $this->id; }
+    public function getName(): string { return $this->name; }
+    public function getCode(): string { return $this->code; }
 }
